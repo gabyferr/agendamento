178b4f5faa1c1ca8062ee5b80ed44f359a7d3979
Gaby <70179462+gabyferr@users.noreply.github.com>
2023-08-31 11:59:43 -0300

    Alterações

--- lib/app/modules/menu/drwer_usuario.dart
+++ lib/app/modules/menu/drwer_usuario.dart
@@ -1,10 +1,23 @@
+import 'package:agendamento/app/modules/usuario/usuario_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import '../usuario/usuario_model.dart';
 
-class DrawerUsuario extends StatelessWidget {
+class DrawerUsuario extends StatefulWidget {
   const DrawerUsuario({super.key});
 
   @override
+  State<DrawerUsuario> createState() => _DrawerUsuarioState();
+}
+
+class _DrawerUsuarioState extends State<DrawerUsuario> {
+  late UsuarioModel usuario;
+  @override
+  void initState() {
+    usuario = UsuarioModel();
+    super.initState();
+  }
+  
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
@@ -74,10 +87,19 @@ class DrawerUsuario extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(
-                  "Minhas informações:",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                ),
+                Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Text(
+                      "Minhas informações:",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(width: 50),
+                    IconButton(onPressed: (){
+                    modalEditar(context);
+                    }, icon: Icon(Icons.edit)),
+                  ],
+                ),     
                 SizedBox(height: 10),
                 Divider(
                   height: 5,
@@ -92,10 +114,6 @@ class DrawerUsuario extends StatelessWidget {
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(15))),
                       labelText: "E-mal:",
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.edit),
-                      ),
                       contentPadding: EdgeInsets.all(2),
                     ),
                   ),
@@ -109,10 +127,6 @@ class DrawerUsuario extends StatelessWidget {
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(15))),
                       labelText: "Celular:",
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.edit),
-                      ),
                       contentPadding: EdgeInsets.all(2),
                     ),
                   ),
@@ -126,10 +140,6 @@ class DrawerUsuario extends StatelessWidget {
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(15))),
                       labelText: "CPF:",
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.edit),
-                      ),
                       contentPadding: EdgeInsets.all(2),
                     ),
                   ),
@@ -143,12 +153,6 @@ class DrawerUsuario extends StatelessWidget {
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(15))),
                       labelText: "Cep:",
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          modalEditar(context);
-                        },
-                        icon: Icon(Icons.edit),
-                      ),
                       contentPadding: EdgeInsets.all(2),
                     ),
                   ),
@@ -163,11 +167,15 @@ class DrawerUsuario extends StatelessWidget {
 }
 
 modalEditar(context) {
+  final _modalEditar = GlobalKey<FormState>();
+  late UsuarioModel usuario;
+
   showDialog(
     context: context,
     builder: (context) => Padding(
-      padding: const EdgeInsets.all(50.0),
+      padding: const EdgeInsets.only(bottom:90, left: 50, right: 50, top: 50,),
       child: Card(
+        key: _modalEditar,
         child: ListView(
           children: [
             Padding(
@@ -180,26 +188,80 @@ modalEditar(context) {
                     "Seus Dados",
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  TextFormField(
+                Column(
+                  children: [
+                          Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+                    child: TextFormField(
+                    strutStyle: StrutStyle(),
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      labelText: "E-mal:",
+                      contentPadding: EdgeInsets.all(2),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+                    child: TextFormField(
+                    strutStyle: StrutStyle(),
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      labelText: "Celular:",
+                      contentPadding: EdgeInsets.all(2),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+                    child: TextFormField(
+                      strutStyle: StrutStyle(),
+                      readOnly: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                      labelText: "CPF:",
+                      contentPadding: EdgeInsets.all(2),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+                  child: TextFormField(
                     strutStyle: StrutStyle(),
                     readOnly: true,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(15))),
                       labelText: "Cep:",
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.edit),
-                      ),
                       contentPadding: EdgeInsets.all(2),
                     ),
                   ),
-                  Container(
-                    child: OutlinedButton(
-                        onPressed: () {
-                          Modular.to.pop();
-                        },
-                        child: Text('Salvar')),
+                ),
+              ],
+            ),
+                 Container(
+                      child: OutlinedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                            MaterialStateProperty.all(Colors.blueAccent)),
+                              onPressed: () {
+                                if (!_modalEditar.currentState!.validate()) {
+                                return;
+                              }
+                              // Modular.get<UsuarioController>().save(usuario);
+                              Modular.to.pop();  
+                              },
+                      child: Text('Salvar',
+                        style: TextStyle(
+                            color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
